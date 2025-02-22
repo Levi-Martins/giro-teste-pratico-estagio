9f08ac8e9c44d50eee06d37de5c1f7f322930c1d
Levi Martins <99222268+Levi-Martins@users.noreply.github.com>
2025-02-22 19:36:02 -0300

    feat: add swagger

---
+++ testePratico/src/main/java/com/giro/testePratico/config/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package com.giro.testePratico.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    public OpenAPI customAPI(){
+        return new OpenAPI().info(new Info().title("API Desafio Técnico Giro").version("1.0"));
+    }
+}
